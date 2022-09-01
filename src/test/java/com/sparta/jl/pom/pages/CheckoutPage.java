cdef918b1750eb4cf68fac1403cbea682ff60294
Jimmy Ng <ngkwoklung@gmail.com>
2022-09-01 12:18:59 +0100

    Init commit

--- src/test/java/com/sparta/jl/pom/pages/CheckoutPage.java
+++ src/test/java/com/sparta/jl/pom/pages/CheckoutPage.java
@@ -1,4 +1,56 @@
 package com.sparta.jl.pom.pages;
 
-public class CheckoutPage extends NavigationPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckoutPage{
+    WebDriver driver;
+
+    By burgerMenu = new By.ById("react-burger-menu-btn");
+    By inventorySidebarLink = new By.ById("react-burger-menu-btn");
+    By continueShoppingBtn = new By.ById("continue-shopping");
+    By inventoryItemName = new By.ById("inventory_item_name");
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+
+        //remove after driver can be passed
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/cart.html"); //remove this after
+    }
+
+    public String getUrl() {
+        return driver.getCurrentUrl();
+    }
+    public HomePage gotoContinueShopping(WebDriver driver) {
+        driver.findElement(continueShoppingBtn).click();
+//        System.out.println(driver.getCurrentUrl());
+//        return null;
+        return new HomePage(driver);
+    }
+
+    public List listOfItems() {
+        List<WebElement> items = driver.findElements();
+        return items;
+    }
+
+    public List listOfRemoveItemBtn() {
+        List<WebElement> removeBtns = driver.findElements(By)
+    }
+
+
+//    public HomePage gotoAllItemsPage(WebDriver driver) {
+//        driver.findElement(burgerMenu).click();
+//        driver.findElement(inventorySidebarLink).click();
+//        return new HomePage(driver);
+//    }
+
+
+
+
 }
---
+++ src/test/java/com/sparta/jl/tests/CheckoutPageTest.java
@@ -0,0 +1,35 @@
+package com.sparta.jl.tests;
+
+import com.sparta.jl.pom.pages.CheckoutPage;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckoutPageTest {
+    static WebDriver driver;
+    private CheckoutPage checkoutPage;
+
+    @BeforeAll
+    static void setupAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver.get("https://www.saucedemo.com/cart.html");
+    }
+
+    @Test
+    @DisplayName("Check continue shopping button returns to HomePage")
+    void checkContinueShoppingButtonReturnsToHomePage() {
+        Assertions.assertEquals("https://www.saucedemo.com/cart.html", checkoutPage.getUrl());
+    }
+
+
+    @AfterAll
+    static void tearDownAll() {
+        driver.quit();
+    }
+
+}
